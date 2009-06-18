d1e263ac1803732ab8b95b2d78d3cf849b044450
Kenneth Lerman <lerman-emc@se-ltd.com>
2009-06-18 12:30:55 -0400

    Added Makefile and README. Did some cleanup.
    Signed-off-by: Kenneth Lerman <lerman-emc@se-ltd.com>

--- GWiz/makesrc.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-# Create a wizard debian release in the current directory
-RELEASE=SOURCE.deb
--- GWiz/wizard2gcode.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-
-wiz2gcode.py -ini ~/emc2-trunk/configs/sim/axis-wiz.ini $1
